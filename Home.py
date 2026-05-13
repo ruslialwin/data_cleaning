714d3ee34ff39ea3be8d0bc88022254a1829b970
Alwin Rusli <alwinrusli06@gmail.com>
2026-05-13 09:28:37 +0700

    Menambah keterangan Menu Production pada halaman Home.py

--- Home.py
+++ Home.py
@@ -11,4 +11,5 @@ Pilih jenis data yang ingin dibersihkan melalui sidebar di sebelah kiri.
 1. **Realisasi**: Cleaning data laporan laba dan rugi dari modul Accounting.
 2. **Processing & Labour**: Data realisasi hanya biaya gaji, overhead dan beban administrasi (exclude biaya penyusutan) dengan penambahan kolom tipe.
 3. **Realisasi Akun Analitik**: Cleaning data artikel jurnal dari modul Accounting.
+4. **Production**: Data laporan produksi harian dari modul Pabrik.
 """)
