399ecbaa35d4014ca9d915fb9e0b9fad7a2e779f
Alwin Rusli <alwinrusli06@gmail.com>
2026-07-28 11:53:03 +0700

    docs: update Home page markdown

--- Home.py
+++ Home.py
@@ -18,6 +18,14 @@ if st.session_state.get("authentication_status"):
 
     st.markdown("""
     Tools otomatisasi cleaning data Excel Odoo.
+    Pilih jenis data yang ingin dibersihkan melalui sidebar di sebelah kiri.
+
+    **Menu yang tersedia:**
+    1. **Realisasi**: Cleaning data laporan laba dan rugi dari modul Accounting.
+    2. **Processing & Labour**: Data realisasi hanya biaya gaji, overhead dan beban administrasi (exclude biaya penyusutan) dengan penambahan kolom tipe.
+    3. **Realisasi Akun Analitik**: Cleaning data artikel jurnal dari modul Accounting.
+    4. **Production**: Data laporan produksi harian dari modul Pabrik.
+    5. **FFB Price**: Mengambil data harga TBS dari modul Odoo.
     """)
 
 elif st.session_state.get("authentication_status") is False:
